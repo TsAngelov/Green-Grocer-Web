b09ccbb8aeb5b56b21add354d194898efc5a44a1
Tsvetomir Angelov <ts.angelov06@gmail.com>
2025-08-04 13:14:25 +0300

    Updated App DbContext

--- GreenGrocerApp.Data/ApplicationDbContext.cs
+++ GreenGrocerApp.Data/ApplicationDbContext.cs
@@ -1,10 +1,13 @@
 ﻿namespace GreenGrocerApp.Data
 {
     using GreenGrocerApp.Data.Models.Users;
+    using GreenGrocerApp.Data.Models.Products;
+    using GreenGrocerApp.Data.Models.Carts;
+    using GreenGrocerApp.Data.Models.Orders;
     using Microsoft.AspNetCore.Identity;
     using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
     using Microsoft.EntityFrameworkCore;
-
+    using System;
     public class ApplicationDbContext
         : IdentityDbContext<ApplicationUser, IdentityRole<Guid>, Guid>
     {
@@ -12,6 +15,49 @@
             : base(options)
         {
         }
+
+        // Users
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+
+        // Products
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
+        public DbSet<Supply> Supplies { get; set; }
+
+        // Carts
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<CartItem> CartItems { get; set; }
+
+        // Orders
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // Set decimapl precision
+            builder.Entity<Product>()
+                .Property(p => p.Price)
+                .HasPrecision(18, 2);
+
+            builder.Entity<OrderItem>()
+                .Property(oi => oi.UnitPrice)
+                .HasPrecision(18, 2);
+
+            builder.Entity<Order>()
+                .Property(o => o.TotalPrice)
+                .HasPrecision(18, 2);
+
+
+            // Protect from cascade delete
+            foreach (var foreignKey in builder.Model.GetEntityTypes()
+                .SelectMany(e => e.GetForeignKeys()))
+            {
+                foreignKey.DeleteBehavior = DeleteBehavior.Restrict;
+            }
+
+        }
     }
 }
-
